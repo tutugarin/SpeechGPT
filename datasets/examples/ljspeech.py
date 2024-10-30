fbf3341a64c9f5df99de1211927dc9aa11b3fa89
necroshine0 <oepetrov0@gmail.com>
2024-10-30 16:55:21 +0300

    add LJSpeech dataset

---
+++ datasets/examples/ljspeech.py
@@ -0,0 +1,33 @@
+import re
+import numpy as np
+from dataclasses import dataclass
+from datasets import load_dataset
+
+
+@dataclass
+class LJSpeechItem:
+    id: str
+    audio: bytes
+    sampling_rate: int
+    file: str
+    text: str
+    normalized_text: str
+    duration: float
+
+
+def get_dataset_iterator_ljspeech(load_audio=True):
+    print("Загружаем датасет 'LJSpeech'...")
+    ds = load_dataset("keithito/lj_speech")
+    print("Датасет 'LJSpeech' был успешно загружен")
+
+    for item in ds['train']:
+        audio_dict = item['audio'] # np.array
+        yield LJSpeechItem(
+            id=item['id'],
+            audio=audio_dict['array'].tobytes() if load_audio else None,
+            sampling_rate=audio_dict['sampling_rate'],
+            file=item['file'],
+            text = item['text'],
+            normalized_text=item['normalized_text'],
+            duration=audio_dict['array'].shape[0] / audio_dict['sampling_rate']
+        )
