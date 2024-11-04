c988d63dcff4b17d91a048ef31ad36811e275b19
Ivan Yakunin <iyakunin202@yandex.ru>
2024-11-04 14:36:16 +0300

    fixed pylint errors 3

--- datasets/examples/covost.py
+++ datasets/examples/covost.py
@@ -1,8 +1,10 @@
+# pylint: disable=R0903
+
 from dataclasses import dataclass
 from typing import Iterator
 
 @dataclass
-class DatasetItem:
+class CovostItem:
     """Класс, представляющий элемент датасета."""
     id: int
     path: str
@@ -21,24 +23,24 @@ class CoVoST2Dataset:
         """
         self.file_paths = file_paths
 
-    def load_data(self, dataset_type: str, n_rows: int = None) -> Iterator[DatasetItem]:
+    def load_data(self, dataset_type: str, n_rows: int = None) -> Iterator[CovostItem]:
         """
         Построчное чтение данных из файла в зависимости от типа датасета.
         
         :param dataset_type: Тип датасета ('train', 'dev', 'test').
         :param n_rows: Ограничение на количество строк для чтения.
-        :return: Итератор с элементами DatasetItem.
+        :return: Итератор с элементами CovostItem.
         """
         file_path = self.file_paths.get(dataset_type)
         if not file_path:
             raise ValueError(f"Путь для {dataset_type} не найден. Проверьте file_paths.")
-        
         # Открываем файл и читаем построчно
         with open(file_path, 'r', encoding='utf-8') as file:
             # Пропускаем заголовок и проверяем нужные колонки
             header = file.readline().strip().split('\t')
-            indices = {col: header.index(col) for col in ['path', 'sentence', 'translation', 'client_id']}
-            
+            indices = {col: header.index(col) 
+                        for col in ['path', 'sentence', 'translation', 'client_id']}
+
             # Чтение строк с ограничением по n_rows
             for row_count, line in enumerate(file):
                 if n_rows is not None and row_count >= n_rows:
@@ -46,7 +48,7 @@ class CoVoST2Dataset:
 
                 # Получаем значения для каждой колонки
                 row = line.strip().split('\t')
-                yield DatasetItem(
+                yield CovostItem(
                     id=row_count,
                     path=row[indices['path']],
                     sentence=row[indices['sentence']],
